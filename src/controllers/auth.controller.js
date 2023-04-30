772a8b5279cc41e7cbc38832930eb332b4990c70
Julia Rocha <juliarralvim@hotmail.com>
2023-04-30 02:37:13 -0300

    add JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,28 @@
-import bcrypt from 'bcrypt';
-import {loginService} from '../services/auth.service.js';
+import bcrypt from "bcrypt";
+import { loginService, generateToken } from "../services/auth.service.js";
 
-const login = async (req,res) =>{
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
-    const {email,password} = req.body;
+  try {
+    const user = await loginService(email);
 
-    try {
-
-        const user = await loginService(email);
-
-        if(!user){
-            return res.status(404).send({message: "User or password not found"})
-        }
-
-        const passwordIsValid =  bcrypt.compareSync(password,user.password);
-
-        if(!passwordIsValid){
-            return res.status(404).send({message: "User or password not found"});
-
-        }
-
-    
-
-         res.send("Login Ok!");
-        
-    } catch (error) {
-        res.status(500).send(error.message);
-        
+    if (!user) {
+      return res.status(404).send({ message: "User or Password not found" });
     }
 
-    
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
 
+    if (!passwordIsValid) {
+      return res.status(404).send({ message: "User or Password not found" });
+    }
+
+    const token = generateToken(user.id)
+
+    res.send({token});
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
 
-export {login};
+export { login };
