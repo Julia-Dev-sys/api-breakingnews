e6f51357a5ef7b783fa4f46c354add25d90a0c9b
Julia Rocha <juliarralvim@hotmail.com>
2023-05-02 18:01:45 -0300

    add pages next and previous

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,37 +1,84 @@
-import {createService,findAllService} from "../services/news.service.js";
+import {createService,findAllService,countNews} from "../services/news.service.js";
 
 
-const create = async (req,res) => {
+export const create = async (req, res) => {
+  try {
+    const { title, text, banner } = req.body;
 
-    try {
-        
-        const {title,text,banner} = req.body;
-        if(!title || !banner || !text){
-            res.status(400).send({
-                message: "Submit all fiels for registration",
-            })
-        }
-
-        await createService({
-            title,
-            text,
-            banner,
-            user: req.userId//{_id: "644d9ffb8264f136509ed8c1"},
-        })
-        res.send(201);
-    } catch (error) {
-        res.status(500).send({message: error.message});
+    if (!title || !banner || !text) {
+      res.status(400).send({
+        message: "Submit all fields for registration",
+      });
     }
-    
 
+    await createService({
+      title,
+      text,
+      banner,
+      user: req.userId,
+    });
+
+    res.send(201);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-const findAll = async(req,res) => {
-    const news = await findAllService();
-    if(news.length === 0){
-        return res.status(400).send({message: "There are no registered news"})
+export const findAll = async (req, res) => {
+  try {
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+      limit = 5;
     }
-    res.send(news);
+
+    if (!offset) {
+      offset = 0;
+    }
+
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl =
+      next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl =
+      previous != null
+        ? `${currentUrl}?limit=${limit}&offset=${previous}`
+        : null;
+
+    if (news.length === 0) {
+      return res.status(400).send({
+        message: "There are no registered news",
+      });
+    }
+    res.send({
+      nextUrl,
+      previousUrl,
+      limit,
+      offset,
+      total,
+  
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user ? item.user.name : '',
+        username: item.user ? item.user.username : '',
+        userAvatar: item.user ? item.user.avatar : '',
+      })),
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-export  {create,findAll};
--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -1,38 +1,28 @@
 import mongoose from "mongoose";
 import userService from "../services/user.service.js";
 
-export const validId = (req,res,next) => {
-    try {
-        const id = req.params.id
+export const validId = (req, res, next) => {
+  const id = req.params.id;
 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(400).send({message: "Invalid ID"});
-    };
-
-    next();
-    } catch (error) {
-        res.status(500).send({message: err.message})
-    }
-
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({ message: "Invalid id!" });
+  }
+  next();
 };
 
-export const validUser = async (req,res,next) => {
-    try {
-        const id = req.params.id
-    const user = await userService.findByIdService(id);
+export const validUser = async (req, res, next) => {
+  const id = req.params.id;
 
-    if(!user){
-        return res.status(400).send({message: "User not found"});
-    };
+  const user = await userService.findByIdUserService(id);
 
-    req.id= id;
-    req.user = user;
+  if (!user) {
+    return res.status(400).send({
+      message: "User not found",
+    });
+  }
 
-    next();
-    } catch (error) {
-        res.status(500).send({message: err.message})
-    }
+  req.id = id;
+  req.user = user;
 
+  next();
 };
-
-export default {validId,validUser};
